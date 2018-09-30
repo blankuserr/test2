50a10d19eac94f7b6afc2e6c481a2fdfe30b2184
blankuserr <1658424473@qq.com>
2018-09-30 10:09:17 +0800

    Struts2 上实现单表增删改查

---
+++ struts9/src/main/java/cap/dao/impl/AdminDAO.java
@@ -0,0 +1,17 @@
+package cap.dao.impl;
+
+import cap.bean.Admin;
+
+import java.util.List;
+
+public interface AdminDAO {
+    List<Admin> findAdmins();
+
+    Admin findById(int id);
+
+    int addAdmin(Admin admin);
+
+    int delAdmin(int id);
+
+    int updateAdmin(Admin admin);
+}
